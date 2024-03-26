3c1d8d47f7ae3eaacc84a51cdf74b92c79c6980f
edvardsk06 <152157821+edvardsk06@users.noreply.github.com>
2024-03-26 13:10:37 +0200

    Add files via upload

--- projekts.py
+++ projekts.py
@@ -1,19 +1,30 @@
 from random import randint
  
 jautajums = {
-    "Latvijas": "Rīga","Lietuvas": "Viļņa","Igaunijas": "Tallina","Kenijas": "Nairobi","Polijas": "Varšava"
+    "Kad sākās Pirmais pasaules karš?": "1914",
+    "Kurā gadā Džons F. Kenedijs tika noslepkavots?": "1963",
+    "Kur notika pirmās vasaras olimpiskās spēles?Ieraksti pilsētu": "Atēnās",
+    "Kurš bija pirmais amerikānis, kurš ieguva Nobela miera balvu?Ieraksti uzvārdu": "Rūzvelts",
+    "Kur atrodas Babilona?": "Irākā",
+    "Kur atrodas Žannas d'Arkas dzimtene? Ieraksti valsti": "Francijā",
+    "Kura notikuma laikā Koreja tika sadalīta 2 valstīs?": "Otrā pasaules kara",
+    "Kura tiek uzskatīta par pirmo cilvēka tehnoloģiju?": "Uguns",
+    "Kad sākās aukstais karš?": "1947",
+    "Kura valsts Āzijā bija ass dalībniece Otrajā pasaules karā?": "Japāna",
+    "Kurš ir slavenais Kubas līderis, kuram pie varas bija 49 gadus?Ieraksti uzvārdu": "Kastro",
+
+
+
 }
 atslegas = list(jautajums.keys()) # parvers atslega spar list
-print(atslegas)
-
-
+# print(atslegas)
 
 skaits = 0
 pareizas_atbildes = 0
-
+print("Sagatavojoies Vēstures eksāmenam! Lai spēli beigtu, lodziņā uzraksti 'beigt'")
 while True:
     gadijums = randint(0, len(atslegas)-1)
-    jaut = input(f"Kāda ir {atslegas[gadijums]} galvaspilsēta?")  # pajautā
+    jaut = input(f" {atslegas[gadijums]}")  # pajautā
     if jaut.lower() == "beigt":
         print("Paldies par spēli!")
         break
